3ef6201f12972fc14888e488b7cea06371cee870
Carlos <carlosborjawebdev@gmail.com>
2025-04-14 17:45:22 +0200

    pull request big cleanup

--- controllers/routeController.js
+++ controllers/routeController.js
@@ -1,4 +1,4 @@
-import { Route, Review } from '../model/index.js';
+import { Route } from '../model/index.js';
 
 export const displayRoutes = async (req, res) => {
     try {
@@ -10,13 +10,67 @@ export const displayRoutes = async (req, res) => {
     }
 }
 
-export const addRoute = async (req, res) => {
+export const addRouteForm = async (req,res) => {
+  const difficulties=["Easy", "Medium", "Hard"];
+  const terrains= ["Rocky", "Sand", "Forest", "Trail", "Snow"];
+  res.render("createRoute", { difficulties, terrains });
+}
+export const addRoute = async (req, res) => { 
     const { difficulty, location, meetingPoint, distance, elevationGain, durationMins, terrainType } = req.body;
     try {
-      await Route.create({ difficulty, location, meetingPoint, distance, elevationGain, durationMins, terrainType });
+      await Route.create({ difficulty, location, meetingPoint:"default", distance, elevationGain, durationMins, terrainType });
       res.redirect('/routes');
     } catch (err) {
       console.error(err);
       res.status(500).send('Error creating route');
     }
+}
+
+export const editRouteForm = async (req, res) => {
+  const difficulties=["Easy", "Medium", "Hard"];
+  const terrains= ["Rocky", "Sand", "Forest", "Trail", "Snow"];
+  const id = req.params.idRoute;
+  const route = await Route.findByPk(id);
+  if (!route) {
+      res.redirect("/route")
+  }
+  res.render("routeEdit", { route, difficulties, terrains });
+}
+
+export const editRoute = async (req, res) => {
+  const id = req.params.idRoute;
+  const { location, durationMins, distance, difficulty, elevationGain, terrainType } = req.body;
+  const route = await Route.findByPk(id);
+  route.location = location;
+  route.durationMins = parseInt(durationMins) || 0;
+  route.elevationGain = elevationGain;
+  route.distance = distance;
+  route.difficulty = difficulty;
+  route.terrainType = terrainType;
+  try {
+      await route.save();
+  } catch (error) {
+      console.error("Error al editar la ruta:", error);
+      res.status(500).send("Error al editar la ruta");
+  }
+  
+  res.redirect("/routes");
+}
+
+export const removeRoute = async (req, res) => {
+  const id = req.params.idRoute;
+
+  const route = await Route.findByPk(id);
+  try {
+      if (route) {
+          await route.destroy();
+      } else {
+          console.log("No se pudo encontrar la ruta");
+          res.redirect("/routes");
+      }
+  } catch (error) {
+      console.error("Error al eliminar la ruta:", error);
+      res.status(500).send("Error al eliminar la ruta");
+  }
+  res.redirect("/routes");
 }
--- routes/routes.js
+++ routes/routes.js
@@ -1,28 +1,35 @@
 import express from 'express';
 const router = express.Router();
 import authenticateToken from '../middleware/authenticateToken.js';
-import { addRoute, displayRoutes } from '../controllers/routeController.js';
+import isAdmin from '../middleware/isAdmin.js';
+import Route from '../model/Route.js';
+import { addRoute, displayRoutes, addRouteForm, editRoute, editRouteForm, removeRoute } from '../controllers/routeController.js';
 router.get('/routes',authenticateToken, async (req, res) => {
   displayRoutes(req,res);
   });
   
-// router.get('/routes/new', authenticateToken, isAdmin, async (req, res) => {
-//     const { tripId } = req.params;
-//     const { review, idAuthor } = req.body;
-  
-//     try {
-//       await Review.create({ review, idAuthor, idTrip: tripId });
-//       res.redirect(`/reviews/${tripId}`);
-//     } catch (err) {
-//       console.error(err);
-//       res.status(500).send('Error adding review');
-//     }
-  
-//     res.render('addRoute');
-//   });
+  router.get('/routes/new', authenticateToken, isAdmin, async (req, res) => {
+    addRouteForm(req, res);
+  });
+
+router.post('/routeAdd', async (req, res) => {
+  addRoute(req, res);
+  });
 
 router.post('/routes', async (req, res) => {
   addRoute(req, res);  
+});
+
+router.get('/routes/edit/:idRoute',authenticateToken, isAdmin, async (req, res) => {
+  editRouteForm(req,res);
+  });
+
+router.post('/routes/edit/:idRoute',authenticateToken, isAdmin, async (req, res) => {
+  editRoute(req,res);
+});
+
+router.post('/routes/delete/:idRoute',authenticateToken, isAdmin, async (req, res) => {
+  removeRoute(req,res);
 });
 
 export default router;
