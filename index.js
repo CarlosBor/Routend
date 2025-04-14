8788ae5edcbe336e2ea73cd45408f53e1c942d29
CarlosBor <carlosborjawebdev@gmail.com>
2025-04-14 22:52:19 +0200

    Add API endpoints

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import tripRoutes from './routes/trips.js';
 import reviewroutes from './routes/reviews.js';
 import photosroutes from './routes/photos.js';
 import adminUsersroutes from './routes/admin.js';
-import apiRouterReviews from './routes/api/reviews.js';
+import apiRouterReviews from './routes/api/routes.js';
 import apiRouterTrips from './routes/api/trips.js';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
@@ -22,8 +22,11 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
+
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
 app.use(setLocals);
 app.use(authRoutes);
 app.use(routeRoutes);
@@ -33,9 +36,9 @@ app.use(photosroutes);
 app.use(adminUsersroutes);
 app.use(apiRouterReviews);
 app.use(apiRouterTrips);
-app.use(cors());
 
-app.use(express.json());
+
+
 
 app.listen(3000, () => {
   console.log('🚀 Server is running on http://localhost:3000 🚀');
--- routes/api/routes.js
+++ routes/api/routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 const router = express.Router();
-import { Trip, Route } from '../../model/index.js';
-router.get('/api/trips', async (req, res) => {
+import { Route } from '../../model/index.js';
+
+router.get('/api/routes', async (req, res) => {
   try {
-    const trips = await Trip.findAll({ include: Route });
-    res.json(trips);
+    const routes = await Route.findAll({});
+    res.json(routes);
   } catch (err) {
     console.log(err);
     res.status(500).json({ error: 'Error fetching trips' });
